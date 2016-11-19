59c5baac6a8abd23ea295bfcd5c7a5710e156c35
Joshua Christiansen <joshua.christiansen@codercamps.com>
2016-11-18 20:33:08 -0700

    initial commit

---
+++ src/GitHubConnect/Program.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GitHubConnect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new RestClient("http://github.com");
+            var request = new RestRequest("/repos/codercampsjosh/FirstGitHub/blob/master/app.js");
+
+            var result = client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
+
